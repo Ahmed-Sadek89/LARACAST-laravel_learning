591a6e38aee0f71a2479fa2eaf7bf61434cbeef8
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-09-17 04:03:58 +0200

    first laravel app LARACAST::WEEK_1

---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get("/jobs", function () {
+    $jobs = [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40,000'
+            ]
+        ]
+    ];
+    return view("jobs", $jobs);
+});
+
+Route::get("/jobs/{id}", function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '$10,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '$40,000'
+        ]
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view("job", ["job" => $job]);
+});
+
+Route::get("/contact", function () {
+    return view("contact");
+});
